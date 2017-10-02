894672d99376385578db16ac08c1dfe4adaca996
jhoke97 <31226128+jhoke97@users.noreply.github.com>
2017-10-02 16:22:26 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
-//Request file name 
-
+//Request file name from user
+//Search directory for file name and load file into program
+/*Convert bitmap image into a vector matrix
+    -Create matrix with size rows of pixels x columns of pixels */
+/*Create Loop That:
+  -Gets RGB values of each pixel then get the average of all three values
+  -Inserts the average of the values into the RGB values of the pixel*/
+//Save file to computer as oldtimey.bmp
 
 #include <iostream>
+#include <vectors>
+#include "bitmap.h"
+
 using namespace std;
 int main(){
   return 0;
