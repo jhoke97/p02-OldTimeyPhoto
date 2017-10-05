3ba1fd49136be2d9d559e3626a3e0fe1adda473c
jhoke97 <31226128+jhoke97@users.noreply.github.com>
2017-10-04 17:52:34 -0700

    Update main.cpp
    Outputs rows and columns. Converts image "tiger.bmp" to greyscale.

--- main.cpp
+++ main.cpp
@@ -8,10 +8,53 @@
 //Save file to computer as oldtimey.bmp
 
 #include <iostream>
-#include <vectors>
+#include <vector>
 #include "bitmap.h"
-
 using namespace std;
-int main(){
+const int RGB_3 = 3;
+int main()
+ {
+  Bitmap image;
+  vector <vector <Pixel> > bmp;
+  Pixel rgb;
+  image.open("tiger.bmp");
+  bmp = image.toPixelMatrix();
+  int rows = 0;
+  int columns = 0;
+    for(int i = 0; i <bmp.size(); i++)
+      {
+        rows = rows + 1;
+          for(int x = 0; x < bmp[i].size(); x++)
+          {
+            columns = columns + 1;
+          }
+      }
+      cout << rows << endl << columns << endl;
+    if(image.isImage())
+     {
+      for(int i = 0; i <rows; i++)
+        {
+          for(int j = 0; j <columns; j++)
+           {
+              rgb = bmp[i][j];
+              int greyScale = (rgb.red +rgb.green +rgb.blue) / RGB_3;
+              rgb.red = greyScale;
+              rgb.green = greyScale;
+              rgb.blue = greyScale;
+              bmp[i][j] = rgb;  
+           }
+
+         }         
+     }
+      else
+     {
+       cout << "Invalid Image" << endl; 
+     }
+  image.fromPixelMatrix(bmp);
+  image.save("oldtimey.bmp"); 
+  
   return 0;
+
 }
+    
+
